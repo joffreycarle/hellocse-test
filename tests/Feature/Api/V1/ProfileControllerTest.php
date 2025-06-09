f48980d090df903480d72634556f897e975f6c90
Joffrey Carle <joffreycarle@gmail.com>
2025-06-09 16:59:50 +0200

    feat: index list profiles administrator with status test

--- tests/Feature/Api/V1/ProfileControllerTest.php
+++ tests/Feature/Api/V1/ProfileControllerTest.php
@@ -6,6 +6,7 @@ use App\Enums\ProfileStatus;
 use App\Models\Administrator;
 use App\Models\Profile;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class ProfileControllerTest extends TestCase
@@ -40,6 +41,37 @@ class ProfileControllerTest extends TestCase
                 ],
             ],
         ]);
+
+        $response->assertJsonCount(10, 'data');
+        // check that only one page of 10 results is returned
+        $response->assertJsonPath('meta.current_page', 1);
+        $response->assertJsonPath('meta.last_page', 1);
+    }
+
+    public function test_administrator_can_list_active_profiles_with_status(): void
+    {
+        Sanctum::actingAs($this->admin);
+
+        $response = $this->get('/api/v1/profiles');
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'first_name',
+                    'last_name',
+                    'image',
+                    'status',
+                ],
+            ],
+        ]);
+
+        // assert that each status element is equal to 'active'
+        collect($response->json('data'))->each(function ($profile) {
+            $this->assertEquals(ProfileStatus::Active->value, $profile['status']);
+        });
 
         $response->assertJsonCount(10, 'data');
         // check that only one page of 10 results is returned
